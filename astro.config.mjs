fef003c4ca67b2bc2f970ae9b6e0fcb01e369e2b
Argjend Haxhiu <haxhiuargjend@gmail.com>
2025-08-25 23:11:13 +0100

    :bug: updated site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  site: 'https://london-banana-checker.netlify.app'
+  site: 'https://londonbanana.co.uk'
 });
