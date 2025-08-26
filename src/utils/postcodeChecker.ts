b157f3c3525aa49c38a905b1534ef5659f29a0bd
Argjend Haxhiu <haxhiuargjend@gmail.com>
2025-08-26 01:26:12 +0100

    :bug: fix postcode checker subareas

--- src/utils/postcodeChecker.ts
+++ src/utils/postcodeChecker.ts
@@ -2,6 +2,7 @@
 export const BANANA_POSTCODES = new Set([
   // North West London (coordinates: -0.270 to -0.167, 51.528 to 51.680)
   'NW1',  // Camden area (coordinate: -0.145, 51.540)
+  'NW1X', // Camden sub-districts
   'NW3',  // Hampstead (coordinate: -0.220, 51.555)
   'NW5',  // Kentish Town (coordinate: -0.145, 51.555)
   'NW7',  // Mill Hill (coordinate: -0.240, 51.615)
@@ -9,6 +10,8 @@ export const BANANA_POSTCODES = new Set([
   
   // North London (coordinates: -0.155 to -0.120, 51.545 to 51.680)
   'N1',   // Islington/Kings Cross (coordinate: -0.115, 51.545)
+  'N1C',  // Kings Cross sub-district
+  'N1P',  // Islington sub-district
   'N2',   // East Finchley (coordinate: -0.165, 51.590)
   'N3',   // Finchley Central (coordinate: -0.195, 51.600)
   'N6',   // Highgate (coordinate: -0.145, 51.575)
@@ -26,13 +29,53 @@ export const BANANA_POSTCODES = new Set([
   
   // Central London - East Central (coordinates: -0.110 to -0.070, 51.515 to 51.540)
   'EC1',  // Finsbury area (coordinate: -0.110, 51.540)
+  'EC1A', // Barbican/City
+  'EC1M', // Farringdon
+  'EC1N', // Hatton Garden
+  'EC1R', // Saffron Hill
+  'EC1V', // Aldersgate
+  'EC1Y', // Clerkenwell
   'EC2',  // Shoreditch area (coordinate: -0.080, 51.525)
+  'EC2A', // Shoreditch
+  'EC2M', // Moorgate
+  'EC2N', // Bank
+  'EC2R', // Bank
+  'EC2V', // Guildhall
+  'EC2Y', // Barbican
   
   // Central London - West Central (coordinates: -0.125, 51.515)
+  'WC1',  // Bloomsbury
+  'WC1A', // Holborn
+  'WC1B', // Bloomsbury
+  'WC1E', // Bloomsbury
+  'WC1H', // Bloomsbury
+  'WC1N', // Bloomsbury
+  'WC1R', // Bloomsbury
+  'WC1V', // Bloomsbury
+  'WC1X', // Kings Cross
   'WC2',  // Covent Garden (coordinate: -0.125, 51.515)
+  'WC2A', // Holborn
+  'WC2B', // Holborn
+  'WC2E', // Covent Garden
+  'WC2H', // Covent Garden
+  'WC2N', // Covent Garden
+  'WC2R', // Covent Garden
   
   // West London (coordinates: -0.220 to -0.175, 51.495 to 51.520)
   'W1',   // West End (coordinate: -0.145, 51.515)
+  'W1A',  // Westminster/West End
+  'W1B',  // Fitzrovia/Marylebone
+  'W1C',  // Fitzrovia
+  'W1D',  // Soho
+  'W1F',  // Fitzrovia
+  'W1G',  // Marylebone
+  'W1H',  // Marylebone (includes W1H 5LG area)
+  'W1J',  // Mayfair
+  'W1K',  // Mayfair
+  'W1S',  // Mayfair
+  'W1T',  // Fitzrovia
+  'W1U',  // Marylebone
+  'W1W',  // Fitzrovia
   'W2',   // Paddington (coordinate: -0.175, 51.520)
   'W6',   // Brook Green/Hammersmith (coordinate: -0.220, 51.495)
   'W10',  // North Kensington (coordinate: -0.215, 51.525)
@@ -41,10 +84,22 @@ export const BANANA_POSTCODES = new Set([
   
   // South West London (coordinates: -0.240 to -0.115, 51.420 to 51.490)
   'SW1',  // Westminster/Belgravia (coordinate: -0.145, 51.500)
+  'SW1A', // Westminster
+  'SW1E', // Belgravia
+  'SW1H', // Westminster
+  'SW1P', // Westminster
+  'SW1V', // Belgravia
+  'SW1W', // Belgravia
+  'SW1X', // Knightsbridge/Belgravia
+  'SW1Y', // Westminster
+  'SW3',  // Chelsea
   'SW4',  // Clapham (coordinate: -0.130, 51.465)
+  'SW5',  // Earls Court
   'SW6',  // Fulham (coordinate: -0.195, 51.480)
+  'SW7',  // South Kensington
   'SW8',  // South Lambeth (coordinate: -0.125, 51.475)
   'SW9',  // Stockwell (coordinate: -0.115, 51.470)
+  'SW10', // Chelsea
   'SW11', // Battersea (coordinate: -0.185, 51.475)
   'SW12', // Balham (coordinate: -0.155, 51.445)
   'SW13', // Barnes (coordinate: -0.240, 51.483)
@@ -55,6 +110,7 @@ export const BANANA_POSTCODES = new Set([
   
   // South East London (coordinates: -0.075 to -0.065, 51.490 to 51.500)
   'SE1',  // Southwark/London Bridge (coordinate: -0.065, 51.500)
+  'SE1P', // Southwark
   
   // Twickenham/Richmond area (coordinates: -0.340 to -0.305, 51.405 to 51.460)
   'TW9',  // Richmond (coordinate: -0.305, 51.460)
